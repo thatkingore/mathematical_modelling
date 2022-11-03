d7c6e1d0cb5295a2f17b5eb402ccf954bc8db0ee
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-03 23:35:01 +0000

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -81,3 +81,77 @@ TEST_CASE("Remainders") {
 TEST_CASE("Sudoku::box") {
 	REQUIRE((Part{ 11, 12, 13, 21, 22, 23, 31, 32, 33 }) == Sudoku{ Test }.box(0));
 }
+
+bool duplicates(const Part& p) 
+{
+	auto counter = Part{ 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	for (auto e : p) {
+		if (e != 0) {
+			++counter[e - 1];
+			if (counter[e - 1] > 1)
+				return true;
+		}
+	}
+	return false;
+}
+
+TEST_CASE("Part contains no duplicates") {
+	REQUIRE(!duplicates(Part{ 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
+	REQUIRE(!duplicates(Part{ 0, 2, 0, 4, 0, 6, 7, 0, 9 }));
+	REQUIRE(duplicates(Part{ 0, 2, 2, 4, 0, 6, 7, 0, 9 }));
+}
+
+static const auto Solved = Grid{
+	Part{ 1, 4, 8, 6, 5, 9, 7, 3, 2 },
+	Part{ 7, 2, 5, 8, 1, 3, 9, 6, 4 },
+	Part{ 9, 6, 3, 4, 7, 2, 5, 1, 8 },
+	Part{ 3, 7, 2, 5, 8, 6, 4, 9, 1 },
+	Part{ 5, 1, 6, 9, 2, 4, 3, 8, 7 },
+	Part{ 8, 9, 4, 1, 3, 7, 2, 5, 6 },
+	Part{ 4, 3, 1, 7, 6, 5, 8, 2, 9 },
+	Part{ 6, 5, 7, 2, 9, 8, 1, 4, 3 },
+	Part{ 2, 8, 9, 3, 4, 1, 6, 7, 5 },
+};
+
+static const auto Partial = Grid{
+	Part{ 1, 4, 0, 0, 0, 9, 0, 3, 0 },
+	Part{ 0, 0, 5, 8, 0, 3, 0, 6, 0 },
+	Part{ 9, 0, 0, 0, 0, 0, 5, 0, 0 },
+	Part{ 3, 7, 0, 5, 0, 0, 4, 0, 0 },
+	Part{ 0, 1, 6, 0, 0, 0, 3, 8, 0 },
+	Part{ 0, 0, 4, 0, 0, 7, 0, 5, 6 },
+	Part{ 0, 0, 1, 0, 0, 0, 0, 0, 9 },
+	Part{ 0, 5, 0, 2, 0, 8, 1, 0, 0 },
+	Part{ 0, 8, 0, 3, 0, 0, 0, 7, 5 },
+};
+
+static const auto Invalid = Grid{
+	Part{ 1, 4, 2, 6, 5, 9, 7, 3, 2 },
+	Part{ 7, 2, 5, 8, 1, 3, 9, 6, 4 },
+	Part{ 9, 6, 3, 4, 7, 2, 5, 1, 8 },
+	Part{ 3, 7, 2, 5, 8, 6, 4, 9, 1 },
+	Part{ 5, 1, 6, 9, 2, 4, 3, 8, 7 },
+	Part{ 8, 9, 4, 1, 3, 7, 2, 5, 6 },
+	Part{ 4, 3, 1, 7, 6, 5, 8, 2, 9 },
+	Part{ 6, 5, 7, 2, 9, 8, 1, 4, 3 },
+	Part{ 2, 8, 9, 3, 4, 1, 6, 7, 5 },
+};
+
+bool sudoku_valid(const Grid& g) {
+	Sudoku s{ g };
+	for (int i = 0; i < 9; ++i) {
+		if	(
+				duplicates(s.row(i)) 
+				|| duplicates(s.column(i)) 
+				|| duplicates(s.box(i))
+			)
+			return false;
+	}
+	return true;
+}
+
+TEST_CASE("(partial) Grid contains no duplicates") {
+	REQUIRE(sudoku_valid(Solved));
+	REQUIRE(sudoku_valid(Partial));
+	REQUIRE(!sudoku_valid(Invalid));
+}
